313669c0481adb857678d32b1bcf0cae5840d728
nikita <shamaev090299@gmail.com>
2022-06-25 16:32:18 +0300

    is_valid_uuid

---
+++ app/utils.py
@@ -0,0 +1,22 @@
+from uuid import UUID
+
+
+def is_valid_uuid(uuid_to_test: str, version: int = 4) -> bool:
+    """
+    Check if uuid_to_test is a valid UUID.
+
+     Parameters
+    ----------
+    uuid_to_test : str
+    version : {1, 2, 3, 4}
+
+     Returns
+    -------
+    `True` if uuid_to_test is a valid UUID, otherwise `False`.
+    """
+
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except ValueError:
+        return False
+    return str(uuid_obj) == uuid_to_test
